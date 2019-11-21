db27069dec9d07cda8223f1f0b10af8885301773
Triston Tetley <tetleylax@aol.com>
2019-11-20 20:07:15 -0500

    added the working function in apiRoutes.js to compare differences in score and find user a loser

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,20 +8,60 @@ module.exports = function (app) {
 
     app.post("/api/survey", function (req, res) {
         if (friendsData) {
+            //value = req.body.scores.reduce(function (a, b) { return a + b }) / 10;
+            //scores(value);
+            //let newestLoser = [];
+            for (let i = 0; i < 10; i++) {
+                req.body.scores[i] = parseInt(req.body.scores[i]);
+                //newestLoser.push(req.body.scores[i]);
+            }
+            friendsData.push(friendsData[differences(req.body.scores)]);
             friendsData.push(req.body);
             res.json(true);
         }
     });
 };
 
-function scores(currentFriendValue) {
-    const allFriendValues = []
-    for (let i = 0; i < friendsData.length; i++) {
-        allFriendValues.push(friendsData[i].scores.reduce(function (a, b) { return a + b }) / 10)
-    }
-    for (let i = 0; i < allFriendValues.length; i++) {
+/* function scores(currentFriendValue) {
+   const allFriendValues = [];
+   let closestFriendDifference = 0;
+   let closestFriend;
+   for (let i = 0; i < friendsData.length; i++) {
+       allFriendValues.push(friendsData[i].scores.reduce(function (a, b) { return a + b }) / 10);
+   }
+   for (let i = 0; i < allFriendValues.length; i++) {
+       if (currentFriendValue - allFriendValues[i] < closestFriend) {
+           closestFriendDifference = currentFriendValue - allFriendValues[i];
+           closestFriend = allFriendValues[i];
+       }
        
+   } 
+
+
+}; */
+
+
+
+
+
+function differences(newestLoser) {
+    let placeHolder;
+    let smallestDiff = 101;
+    let closestFriend;
+    for (let i = 0; i < friendsData.length; i++) {
+        placeHolder = 0;
+        for (let j = 0; j < 10; j++) {
+            placeHolder += Math.abs(friendsData[i].scores[j] - parseInt(newestLoser[j]));
+            
+        }
+        if (placeHolder < smallestDiff) {
+            smallestDiff = placeHolder;
+            closestFriend = i;
+
+        }
     }
     
-}
+    return closestFriend;
+};
+
 
